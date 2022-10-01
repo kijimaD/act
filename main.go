93e77ba1ad5eb2cc38a03de235057863e06244bc
kijimaD <norimaking777@gmail.com>
2022-10-02 00:39:51 +0900

    feat: use api token

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import(
 	"github.com/google/go-github/v47/github"
 	"fmt"
 	"context"
+	"os"
+	"golang.org/x/oauth2"
 )
 
-type Stats struct {
+type Repo struct {
 	Name string
 	Description string
 	Language string
@@ -14,28 +16,55 @@ type Stats struct {
 	DefaultBranch string
 	ForksCount int
 	StargazersCount int
+	CommitCount int
 }
 
 func main() {
-	client := github.NewClient(nil)
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GH_TOKEN")},
+	)
+	ctx := context.Background()
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
 
-	// list all organizations for user
-	orgs, _, err := client.Organizations.List(context.Background(), "kijimaD", nil)
-	if err != nil{
-		panic("error")
+	repos, _ , _ := client.Repositories.List(ctx, "kijimaD", nil)
+	for _, r := range repos {
+		contributors, _, err := client.Repositories.ListContributorsStats(context.Background(), "kijimaD", *r.Name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(string(*r.Name))
+		fmt.Println(*contributors[0].Total)
+		repo := Repo{
+			// string(*r.Name),
+			"a",
+			// string(*r.Description),
+			"a",
+			// string(*r.Language),
+			"a",
+			// *r.HTMLURL,
+			"a",
+			// *r.DefaultBranch,
+			"a",
+			// *r.ForksCount,
+			0,
+			// *r.StargazersCount,
+			0,
+			// *contributors[0].Total,
+			0,
+		}
+		fmt.Println(repo)
 	}
-	fmt.Println(orgs)
 
-	repos, _ , _ := client.Repositories.List(context.Background(), "kijimaD", nil)
-	fmt.Println(*repos[0])
-	fmt.Println(*repos[0].Name)
-	fmt.Println(*repos[0].Description)
-	fmt.Println(*repos[0].Language)
-	fmt.Println(*repos[0].HTMLURL)
-	fmt.Println(*repos[0].DefaultBranch)
-	fmt.Println(*repos[0].ForksCount)
-	fmt.Println(*repos[0].StargazersCount)
-
-	stats, _, _ := client.Repositories.ListContributorsStats(context.Background(), "kijimaD", *repos[0].Name)
-	fmt.Println(*stats[0].Total)
+	// repo := Repo{
+	// 	*repos[0].Name,
+	// 	*repos[0].Description,
+	// 	*repos[0].Language,
+	// 	*repos[0].HTMLURL,
+	// 	*repos[0].DefaultBranch,
+	// 	*repos[0].ForksCount,
+	// 	*repos[0].StargazersCount,
+	// 	*contributors[0].Total,
+	// }
 }
