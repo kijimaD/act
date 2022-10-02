13d1efcd1a5acd46c98ce92d8bd6b74d3d30ffe8
kijimaD <norimaking777@gmail.com>
2022-10-02 21:36:55 +0900

    feat: commit option

--- config.go
+++ config.go
@@ -9,14 +9,19 @@ var DefaultConfigFilePath = ".act.yml"
 
 // memo: 各フィールドはpublicである必要がある
 type Config struct {
-	OutType string `yaml:"outType"`
-	OutPath string `yaml:"outPath"`
+	OutType  string `yaml:"outType"`
+	OutPath  string `yaml:"outPath"`
+	IsCommit bool   `yaml:"commit"`
+	IsPush   bool   `yaml:"push"`
 }
 
 func newConfig() Config {
+	// default parameter
 	return Config{
 		OutType: "stdout",
 		OutPath: "./README.md",
+		IsCommit:  false,
+		IsPush:    false,
 	}
 }
 
